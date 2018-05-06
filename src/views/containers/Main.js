1e24b2c80f46bf38684ea9b81e2d1fa9b6f55d65
Steven Yi <stevenyi@gmail.com>
2018-05-05 20:48:35 -0400

    working on getting codemirror integrated

--- src/views/containers/Main.js
+++ src/views/containers/Main.js
@@ -1,23 +1,39 @@
 import React from "react";
+import CodeMirror from "react-codemirror"
 import { connect } from "react-redux";
 import { Card, CardHeader, CardBody, Row, Col } from "reactstrap";
 import { getAll } from "../../selectors/FirebaseSelectors";
 
+// this didn't quite work...
+//import * as csmode from '../../mode/csound/csound.js';
+
+require('codemirror/addon/comment/comment.js');
+require('codemirror/addon/edit/matchbrackets.js');
+require('codemirror/addon/edit/closebrackets.js');
+require('codemirror/keymap/vim.js');
+require('codemirror/keymap/emacs.js');
+
+
+require('codemirror/lib/codemirror.css');
+require('codemirror/theme/monokai.css');
+
 class Main extends React.Component {
+
+    constructor() {
+        super()
+        this.state = { code: ";; Csound code"}
+    }
+
     render() {
+        let options = { 
+            lineNumbers: true,
+            matchBrackets: true,
+            autoCloseBrackets: true,
+            theme: 'monokai',
+            mode: 'csound',
+        }
         return (
-            <div>
-                <Row>
-                    <Col>
-                        <Card>
-                            <CardHeader>Main</CardHeader>
-                            <CardBody>
-                                {JSON.stringify(this.props.allData)}
-                            </CardBody>
-                        </Card>
-                    </Col>
-                </Row>
-            </div>
+            <CodeMirror value={this.state.code}  options={options}/>
         );
     }
 }
