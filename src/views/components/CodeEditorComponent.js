b92235b7db4bd7fd2a73aeca7b258898468a9f96
Steven Yi <stevenyi@gmail.com>
2018-05-17 18:09:38 -0400

    got editor ref worked out and commands triggering from keyboard

--- src/views/components/CodeEditorComponent.js
+++ src/views/components/CodeEditorComponent.js
@@ -10,16 +10,41 @@ require("codemirror/keymap/emacs.js");
 require("codemirror/lib/codemirror.css");
 require("codemirror/theme/monokai.css");
 
+
 class CodeEditorComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.cm = React.createRef();
+    }
+
+    evalCode() { 
+        let editor = this.cm.current.getCodeMirror(); 
+        console.log("Eval code: " + editor.getSelection());
+        // TODO - hook this into global CsoundObj instance?
+    }
+
+    toggleComment() { 
+        let editor = this.cm.current.getCodeMirror(); 
+        console.log(editor);
+        editor.toggleComment(); 
+    }
+    
     render() {
         let options = {
             lineNumbers: true,
             matchBrackets: true,
             autoCloseBrackets: true,
             theme: "monokai",
-            mode: "csound"
+            mode: "csound",
+            extraKeys: {
+                "Ctrl-E": () => this.evalCode(),
+                // "Ctrl-H": insertHexplay,
+                // "Ctrl-J": insertEuclidplay,
+                "Ctrl-;": () => this.toggleComment(),
+            },
         };
-        return <CodeMirror value={this.props.code} options={options} />;
+        return <CodeMirror value={this.props.code} options={options} 
+                           ref={this.cm} />;
     }
 }
 
