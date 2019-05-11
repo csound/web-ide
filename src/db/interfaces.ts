80485c1f0701fc1d1ad30fa017a11201a8588499
hlolli <hlolli@gmail.com>
2019-05-11 20:23:25 +0200

    data modeling starting point

--- src/db/interfaces.ts
+++ src/db/interfaces.ts
@@ -1,10 +1,40 @@
+export interface IAssetFile {
+    name: string;
+    isBinary: boolean;
+    lastEdit: Date;
+}
+
+export interface ICsoundFile {
+    name: string;
+    lastEdit: Date;
+}
+
 export interface IProject {
+    projectId: number;
+    assets: IAssetFile[];
     name: string;
     isPublic: boolean;
+    files: ICsoundFile[];
+}
+
+export interface ICollection {
+    collectionId: number;
+    created: Date;
+    name: string;
+    lastEdit: Date;
+    projects: IProject[];
+}
+
+export interface IUserProfile {
+    name: string;
+    email: string;
+    collections: ICollection[];
+    userid: number;
 }
 
 export interface IStore {
     router: any;
     LoginReducer: any;
     RouterReducer: any;
+    userProfile: IUserProfile | null;
 }
