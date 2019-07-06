c2c90a93108118d60525f8bbb0e7dc514b3e58e1
Steven Yi <stevenyi@gmail.com>
2019-07-06 16:51:40 -0400

    modified style to be dark background with lighter text/icons (needs fix for treeIcon)

--- src/components/FileTree/styles.tsx
+++ src/components/FileTree/styles.tsx
@@ -5,13 +5,20 @@ const useStyles = makeStyles({
     container: {
         width: "100%",
         height: "100%",
+        background: "#222",
+    },
+    // TODO - SYY - This doesn't work, not sure how to get the treeIcon color styled...
+    treeIcon: {
+        color: "rgb(222,222,222)",
     },
     icon: {
-        fontSize: 20
+        fontSize: 20,
+        color: "rgb(222,222,222)",
     },
     node: {
         display: "flex",
-        alignContent: "center"
+        alignContent: "center",
+        color: "rgb(222,222,222)",
     }
 });
 
