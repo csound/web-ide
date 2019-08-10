ecc60502901900e69cabe20d52b4c387c84b150b
Steven Yi <stevenyi@gmail.com>
2019-08-10 13:39:49 -0400

    added default Csound project files

--- src/components/Projects/reducer.ts
+++ src/components/Projects/reducer.ts
@@ -5,12 +5,58 @@ export interface IProjectsReducer {
     projects: IProject[];
 };
 
-const defaultInitialDocumentValue: IDocument = {
+const defaultCsd: IDocument = {
     currentValue: "",
     lastEdit: null,
-    name: "untitled.csd",
-    savedValue: "",
+    name: "project.csd",
+    savedValue: `<CsoundSynthesizer>
+<CsOptions>
+-o dac
+</CsOptions>
+<CsInstruments>
+#include "project.orc"
+</CsInstruments>
+<CsScore>
+#include "project.sco"
+</CsScore>
+</CsoundSynthesizer>
+    `,
     type: "csd",
+}
+
+const defaultOrc: IDocument = {
+    currentValue: "",
+    lastEdit: null,
+    name: "project.orc",
+    savedValue: `
+sr=44100
+ksmps=32
+0dbfs=1
+
+instr 1
+  iamp = ampdbfs(p5)
+  ipch = cps2pch(p4,12)
+  ipan = 0.5
+  
+  asig = vco2(iamp, ipch)
+
+  al, ar pan2 asig, ipan 
+
+  out(al, ar)
+endin
+
+    `,
+    type: "orc",
+}
+
+const defaultSco: IDocument = {
+    currentValue: "",
+    lastEdit: null,
+    name: "project.sco",
+    savedValue: `
+i1 0 2 8.00 -12
+    `,
+    type: "sco",
 }
 
 const initialProjectsState: IProjectsReducer = {
@@ -18,7 +64,7 @@ const initialProjectsState: IProjectsReducer = {
         {
             name: "Untitled Project",
             isPublic: false,
-            documents: [defaultInitialDocumentValue],
+            documents: [defaultCsd, defaultOrc, defaultSco],
             projectId: 0,
             assets: [],
         }
