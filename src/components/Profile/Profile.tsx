10055e972baf528449be590ab3e494731d1a6ae1
Steven Yi <stevenyi@gmail.com>
2019-08-25 14:20:53 -0400

    implemented adding of new project from Profile (Needs default documents to be added to project too...)

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,28 +3,57 @@ import withStyles from "./styles";
 import { Link } from 'react-router-dom'
 import Header from "../Header/Header";
 import { projects } from "../../config/firestore";
-import { IProject } from "../Projects/interfaces";
+import * as firebase from "firebase/app";
+import { generateUid } from "../../utils";
 
-class Profile extends Component<any, {}> {
+
+interface IProfileState {
+    projects: Array<any>;
+    dataLoaded:boolean;
+}
+
+class Profile extends Component<any, IProfileState> {
+
+    constructor(props) {
+        super(props);
+        this.state = {projects: [], dataLoaded: false};
+    }
+
+    public componentDidMount() {
+        // FIXME
+        // UID needs to be determined from url if a username is given, 
+        // or default to authorized user
+        let currentUser = firebase.auth().currentUser;
+
+        if(currentUser != null) {
+            let uid = firebase.auth().currentUser.uid;
+
+            // Need to fix this...
+            projects.where("userUid", "==", uid).get()
+            .then(querySnapshot => {
+               // FIXME: Must be a better way...
+               let projects = [];
+               querySnapshot.forEach(d => projects.push(d.data()));
+               this.setState({projects, dataLoaded: true})
+            });
+
+        }
+
+    }
     public render() {
 
-        // Need to fix this...
-        let userProjects = projects.where("uid", "==", "0");
-        console.log("userProjects:", userProjects);
+        let projectLinks = [<li key="-1">Loading projects...</li>];
 
-        let fakeProjects: IProject[] = [1,2,3].map((d) => {
-            return {
-                projectUid: d + "",
-                assets:[],
-                name: "Project " + d,
-                isPublic: true,
-                documents: {},
+        if(this.state.dataLoaded) {
+        let projects = this.state.projects;
+            projectLinks = [<li key="0">No Projects found for user.</li>];
+            
+            if(projects != null && projects.length > 0) {
+                projectLinks = projects.map(doc =>
+                    <li key={doc.projectUid}><Link to={"/editor/" + doc.projectUid}>{doc.name}</Link></li>
+                );
             }
-        });
-
-        let projectLinks = fakeProjects.map(p =>
-            <li key={p.projectUid}><Link to={"/editor/username/" + p.projectUid}>{p.name}</Link></li>
-        );
+        }
 
         const { classes } = this.props;
         return (
@@ -34,16 +63,47 @@ class Profile extends Component<any, {}> {
                     <h1>Profile</h1>
                     <p> </p>
                     <p>
-                        <Link to="/editor">
-                            + New Project
+                        <Link to="" onClick={this.createNewProject} >
+                            + Create New Project
                         </Link>
                     </p>
+                    <h2>User Projects</h2>
                     <ul>
                         { projectLinks }
                     </ul>
                 </main>
             </div>
         )
+    }
+
+    createNewProject(e:React.MouseEvent<HTMLAnchorElement,MouseEvent>) {
+        e.stopPropagation();
+        e.preventDefault();
+        // FIXME - use a different filename
+        let docId = generateUid("project"); 
+
+        // FIXME - check if current user is == to user profile being viewed
+        let currentUser = firebase.auth().currentUser;
+
+        if(currentUser != null) {
+            let uid = firebase.auth().currentUser.uid;
+
+            let newProject = {
+                userUid: uid,
+                projectUid: docId,
+                name: "New Project",
+                public: false,
+            };
+
+            projects.doc(docId).set(newProject)
+            .then(() => {
+                
+            })
+            .catch(err => {
+
+            });
+
+        }
     }
 }
 
