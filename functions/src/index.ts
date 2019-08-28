b4f54cbf1f3f5244232d85e1bdb999ef18e3a2c2
Steven Yi <stevenyi@gmail.com>
2019-08-28 14:43:31 -0400

    added firebase function to create doc in /profiles on user create

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +7,27 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+
+admin.initializeApp();
+
+export const accountCreate = functions.auth.user().onCreate(user => {
+    const userUid = user.uid;
+
+    const doc = { userUid,
+                bio: '',
+                link1:'',
+                link2:'', 
+                link3:'',
+                username: ''};
+
+    admin.firestore().collection('profiles').doc(userUid)
+    .set(doc).then(writeResult => {
+        console.log('User Created result:', writeResult);
+        return;
+    }).catch(err => {
+       console.log(err);
+       return;
+    });
+});
