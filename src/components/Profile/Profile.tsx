be622dc0a92531b8e6288b9f7555e5f3f8854926
Steven Yi <stevenyi@gmail.com>
2019-09-01 12:28:09 -0400

    removed unused var

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import withStyles from "./styles";
-import { Link } from "react-router-dom";
 import Header from "../Header/Header";
 import {
     getUserProjects,
