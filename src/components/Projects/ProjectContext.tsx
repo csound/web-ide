37c5178ed12e29119161c5d7868b4b4e3b609906
Steven Yi <stevenyi@gmail.com>
2019-09-02 13:07:24 -0400

    fix issue with setting up firestore/EMFS sync before CsoundObj loaded by waiting on CsoundObj to be present in Redux store; rewrote component as functional component + hooks

--- src/components/Projects/ProjectContext.tsx
+++ src/components/Projects/ProjectContext.tsx
@@ -1,54 +1,38 @@
-import React from "react";
-import { connect } from "react-redux";
-import { IProject } from "./types";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { IStore } from "../../db/interfaces";
 import { loadProjectFromFirestore } from "./actions";
 
 interface IProjectContextProps {
-    activeProject: IProject;
     className: string;
     children: JSX.Element[] | JSX.Element;
-    loadProjectFromFirestore: (projectUid: string) => void;
     match: any;
 }
 
-class ProjectContext extends React.Component<IProjectContextProps, {}> {
-    componentDidMount() {
-        const projectUid = this.props.match.params.id;
-        if (projectUid) {
-            // FIXME: get project from Firebase
-            this.props.loadProjectFromFirestore(projectUid);
-        } else {
+export const ProjectContext = (props: IProjectContextProps) => {
+    const dispatch = useDispatch();
+    const projectUid = props.match.params.id;
+    const { className } = props;
+    const activeProject = useSelector(
+        (store: IStore) => store.projects.activeProject
+    );
+    const csound = useSelector((store: IStore) => store.csound.csound);
+
+    useEffect(() => {
+        if (csound && !activeProject) {
+            dispatch(loadProjectFromFirestore(projectUid));
         }
+    }, [dispatch, csound, activeProject, projectUid]);
+
+    if (activeProject && csound) {
+        return <main className={className}>{props.children}</main>;
+    } else {
+        return (
+            <main>
+                <h1>LOADING...</h1>
+            </main>
+        );
     }
-
-    render() {
-        const { activeProject, className } = this.props;
-
-        if (activeProject) {
-            return <main className={className}>{this.props.children}</main>;
-        } else {
-            return (
-                <main>
-                    <h1>LOADING...</h1>
-                </main>
-            );
-        }
-    }
-}
-
-const mapStateToProps = (store: IStore, ownProp: any) => {
-    return {
-        activeProject: store.projects.activeProject
-    };
 };
 
-const mapDispatchToProps = (dispatch: any): any => ({
-    loadProjectFromFirestore: (projectUid: string) =>
-        dispatch(loadProjectFromFirestore(projectUid))
-});
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(ProjectContext);
+export default ProjectContext;
