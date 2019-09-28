d997decc70e39f823bba7641abeae91d35b772ac
Edward Costello <phasereset@gmail.com>
2019-09-28 15:27:26 -0700

    Added firebase functions config environmental variables for storagebucket url.

--- functions/src/project_file_storage_delete.js
+++ functions/src/project_file_storage_delete.js
@@ -3,7 +3,7 @@ const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const log = require("./logger.js")("new_user_callback");
 
-// Delete an associated binary file when a project file entry is deleted 
+// Delete an associated binary file when a project file entry is deleted
 const project_file_storage_delete = async binaryUrl => {
     log(
         "project_file_storate_delete",
@@ -12,35 +12,31 @@ const project_file_storage_delete = async binaryUrl => {
 
     return await admin
         .storage()
-        .bucket('csound-ide.appspot.com')
+        .bucket(functions.config().storagebucket.url)
         .file(binaryUrl)
         .delete();
 };
 
-exports.project_file_storage_delete_callback = 
-functions
-  .firestore
-  .document("projects/{projectId}/files/{fileId}")
-  .onDelete((async (snapshot, context) => {
-     const params = context.params;
-     const uid = snapshot.data().userUid;
-     const filetype = snapshot.data().type;
-     const binaryUrl = `${uid}/${params.projectId}/${params.fileId}`;
-
-     log(
-            "project_file_storage_delete_callback",
-            `delete: ${filetype} ${binaryUrl}`
-     )
-
-    if(uid && filetype === "bin") {
+exports.project_file_storage_delete_callback = functions.firestore
+    .document("projects/{projectId}/files/{fileId}")
+    .onDelete(async (snapshot, context) => {
+        const params = context.params;
+        const uid = snapshot.data().userUid;
+        const filetype = snapshot.data().type;
+        const binaryUrl = `${uid}/${params.projectId}/${params.fileId}`;
 
         log(
             "project_file_storage_delete_callback",
-            `Deleting binary file: ${binaryUrl}`
+            `delete: ${filetype} ${binaryUrl}`
         );
-        await project_file_storage_delete(binaryUrl);
 
-    } 
+        if (uid && filetype === "bin") {
+            log(
+                "project_file_storage_delete_callback",
+                `Deleting binary file: ${binaryUrl}`
+            );
+            await project_file_storage_delete(binaryUrl);
+        }
 
         return true;
-    }));
+    });
