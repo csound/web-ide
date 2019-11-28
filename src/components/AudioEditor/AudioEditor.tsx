650afde87c60f14c4dee430f78c416283bfc5a5b
Steven Yi <stevenyi@gmail.com>
2019-11-28 18:40:07 -0500

    added missing AudioEditor

---
+++ src/components/AudioEditor/AudioEditor.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { storageRef } from "../../config/firestore";
+import { useEffect, useState } from "react";
+
+type AudioEditorProps = {
+    audioFileUrl:string,
+}
+
+export const AudioEditor = ({audioFileUrl}:AudioEditorProps) => {
+    const [data, setData] = useState(null);
+    console.log("AFURL: " + audioFileUrl)
+    console.log("Data: " + data)
+    useEffect(() => {
+        (async () => {
+        const fileUrl = await storageRef
+                        .child(audioFileUrl)
+                        .getDownloadURL();
+        setData(fileUrl);
+        })();
+    }, []);
+    return data == null ?
+        <div>
+            <p>Looking up audio file URL...</p>
+        </div> 
+        :
+        <div>
+            <audio controls>
+                <source src={data!}/>
+            </audio>
+        </div>
+        ;
+};
+
+export default AudioEditor;
