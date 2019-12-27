1c658f7838c5e5a36d1caebf341f3a65b5bfb163
hlolli <hlolli@gmail.com>
2019-12-27 02:19:14 +0100

    fix selectActiveProject

--- src/components/Projects/selectors.ts
+++ src/components/Projects/selectors.ts
@@ -1,8 +1,11 @@
 import { IStore } from "../../db/interfaces";
 import { IProject, IProjectsReducer } from "./types";
-import { propOr } from "ramda";
+import { pathOr, propOr } from "ramda";
 
 export const selectActiveProject = (store: IStore): IProject | null => {
     const state: IProjectsReducer = store.ProjectsReducer;
-    return propOr(null, "activeProject", state);
+    const activeProjectUid: string | null = propOr(null, "activeProjectUid", state);
+    return activeProjectUid
+        ? pathOr(null, ["projects", activeProjectUid], state)
+        : null;
 };
