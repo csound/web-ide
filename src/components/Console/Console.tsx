51c344c904c95447a79209d066212b4e7eb3d379
Steven Yi <stevenyi@gmail.com>
2020-02-16 09:39:02 -0500

    fix formatting issue with console output and copy/paste to have newlines

--- src/components/Console/Console.tsx
+++ src/components/Console/Console.tsx
@@ -64,7 +64,7 @@ const Console = ({ width, height }: IConsoleProps) => {
                 ) {
                     setTimeout(() => row.scrollToRow(row.props.rowCount), 9);
                 }
-                return append(msg, currentLogs);
+                return append(msg + '\n', currentLogs);
             });
         }
     };
@@ -85,7 +85,7 @@ const Console = ({ width, height }: IConsoleProps) => {
     function rowRenderer({ key, index, style }) {
         return (
             <li key={key} style={style}>
-                {logs[index]}
+               <pre>{logs[index]}</pre>
             </li>
         );
     }
