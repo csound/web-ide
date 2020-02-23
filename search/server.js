8fcc6c8109887494c10851e6805885248de4a1e3
Edward Costello <phasereset@gmail.com>
2020-02-23 15:58:33 +0000

    Recommit search code

--- search/server.js
+++ search/server.js
@@ -2,12 +2,36 @@ const express = require("express");
 const cors = require("cors");
 
 const port = 4000;
-const startServer = searchCallback => {
+const startServer = (searchCallback, listCallback) => {
     const app = express();
     app.use(cors());
-    app.get("/search/:collection/:query", (req, res) => {
-        const { collection, query } = req.params;
-        const result = searchCallback(collection, query);
+    app.get(
+        "/search/:collection/:query/:count/:offset/:orderKey/:order",
+        (req, res) => {
+            const {
+                collection,
+                query,
+                count,
+                offset,
+                orderKey,
+                order
+            } = req.params;
+            const result = searchCallback(
+                collection,
+                query,
+                count,
+                offset,
+                orderKey,
+                order
+            );
+
+            return res.send(result);
+        }
+    );
+
+    app.get("/list/:collection/:count/:offset/:orderKey/:order", (req, res) => {
+        const { collection, count, offset, orderKey, order } = req.params;
+        const result = listCallback(collection, count, offset, orderKey, order);
 
         return res.send(result);
     });
