80dd70eca60990cbd31e82ce7ce1649f4b6283a7
Edward Costello <phasereset@gmail.com>
2020-02-23 15:25:47 +0000

    Added list query and memoisation

--- search/main.js
+++ search/main.js
@@ -1,19 +1,42 @@
 const cron = require("node-cron");
 const { startServer } = require("./server");
 const { getDatabase } = require("./database");
-const { createDatabaseSearch } = require("./search");
+const {
+    createDatabaseSearch,
+    searchResultFilter,
+    createDatabaseList
+} = require("./search");
 
 const main = async () => {
     let database = await getDatabase();
     let databaseSearch = createDatabaseSearch(database);
+    let databaseList = createDatabaseList(database);
     cron.schedule("0 */2 * * *", async () => {
         database = await getDatabase();
         databaseSearch = createDatabaseSearch(database);
+        databaseList = createDatabaseList(database);
     });
 
-    startServer((collection, query) => {
-        return databaseSearch(collection, query);
-    });
+    startServer(
+        (collection, query, count, offset, orderKey, order) => {
+            let result = databaseSearch(collection, query);
+            const totalRecords = result.length;
+            result = searchResultFilter(result, count, offset, orderKey, order);
+            return {
+                data: result,
+                totalRecords
+            };
+        },
+        (collection, count, offset, orderKey, order) => {
+            let result = databaseList(collection);
+            const totalRecords = result.length;
+            result = searchResultFilter(result, count, offset, orderKey, order);
+            return {
+                data: result,
+                totalRecords
+            };
+        }
+    );
 };
 
 main();
--- search/search.js
+++ search/search.js
@@ -1,4 +1,5 @@
 const Fuse = require("fuse.js");
+const memoize = require("fast-memoize");
 
 const createFuzzySearch = (database = {}, key = "", options = {}) => {
     if (
@@ -19,26 +20,100 @@ const createDatabaseSearch = (database = {}) => {
         keys: ["username", "displayName", "bio"]
     });
     const tags = createFuzzySearch(database, "tags", { keys: ["id"] });
-    const stars = createFuzzySearch(database, "stars", {});
+    const searchMap = { projects, profiles, tags };
 
-    const searchMap = { projects, profiles, tags, stars };
-
-    return (collection = "", query = "") => {
+    return memoize((collection = "", query = "") => {
         if (
-            collection === "" ||
-            query === "" ||
             typeof collection !== "string" ||
             typeof query !== "string" ||
-            typeof searchMap[collection] === "undefined"
+            typeof searchMap[collection] === "undefined" ||
+            collection === "" ||
+            query === ""
         ) {
-            return false;
+            return [];
         }
 
         const result = searchMap[collection].search(query);
+        return result;
+    });
+};
+
+const compareValues = (key, order = "asc") => {
+    return (a, b) => {
+        if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+            return 0;
+        }
+
+        const varA = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
+        const varB = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
+
+        let comparison = 0;
+        if (varA > varB) {
+            comparison = 1;
+        } else if (varA < varB) {
+            comparison = -1;
+        }
+        return order === "desc" ? comparison * -1 : comparison;
+    };
+};
+
+const orderArrayByKey = memoize((array, orderKey, order) => {
+    if (
+        Array.isArray(array) &&
+        array.length > 0 &&
+        orderKey !== false &&
+        order !== false &&
+        typeof orderKey === "string" &&
+        typeof array[0][orderKey] !== "undefined" &&
+        typeof order === "string"
+    ) {
+        array.sort(compareValues(orderKey, order));
+    }
+
+    return array;
+});
+
+const searchResultFilter = memoize(
+    (result = [], count = 0, offset = 0, orderKey = false, order = false) => {
+        if (count !== false && offset !== false) {
+            count = parseInt(count);
+            offset = parseInt(offset);
+        }
+
+        if (
+            Array.isArray(result) === false ||
+            result.length === 0 ||
+            typeof count !== "number" ||
+            typeof offset !== "number" ||
+            count === 0 ||
+            offset >= result.length
+        ) {
+            return [];
+        }
+
+        let sortResult = orderArrayByKey(result, orderKey, order);
+        sortResult = sortResult.slice(offset, offset + count);
+        return sortResult;
+    }
+);
+
+const createDatabaseList = (database = {}) => {
+    return (collection = "") => {
+        if (
+            typeof collection !== "string" ||
+            typeof database[collection] === "undefined" ||
+            collection === ""
+        ) {
+            return [];
+        }
+
+        const result = database[collection];
         return result;
     };
 };
 
 module.exports = {
-    createDatabaseSearch
+    createDatabaseSearch,
+    searchResultFilter,
+    createDatabaseList
 };
