1d579a637e02c23659fa1652366790baae10860e
Edward Costello <phasereset@gmail.com>
2020-02-23 15:27:35 +0000

    Revert "Added list query and memoisation"
    This reverts commit 28c63ccfbcc8220d30ca6c8f38f114a9d24a5c8f.

--- search/search.js
+++ search/search.js
@@ -1,5 +1,4 @@
 const Fuse = require("fuse.js");
-const memoize = require("fast-memoize");
 
 const createFuzzySearch = (database = {}, key = "", options = {}) => {
     if (
@@ -20,100 +19,26 @@ const createDatabaseSearch = (database = {}) => {
         keys: ["username", "displayName", "bio"]
     });
     const tags = createFuzzySearch(database, "tags", { keys: ["id"] });
-    const searchMap = { projects, profiles, tags };
+    const stars = createFuzzySearch(database, "stars", {});
 
-    return memoize((collection = "", query = "") => {
+    const searchMap = { projects, profiles, tags, stars };
+
+    return (collection = "", query = "") => {
         if (
+            collection === "" ||
+            query === "" ||
             typeof collection !== "string" ||
             typeof query !== "string" ||
-            typeof searchMap[collection] === "undefined" ||
-            collection === "" ||
-            query === ""
+            typeof searchMap[collection] === "undefined"
         ) {
-            return [];
+            return false;
         }
 
         const result = searchMap[collection].search(query);
-        return result;
-    });
-};
-
-const compareValues = (key, order = "asc") => {
-    return (a, b) => {
-        if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
-            return 0;
-        }
-
-        const varA = typeof a[key] === "string" ? a[key].toUpperCase() : a[key];
-        const varB = typeof b[key] === "string" ? b[key].toUpperCase() : b[key];
-
-        let comparison = 0;
-        if (varA > varB) {
-            comparison = 1;
-        } else if (varA < varB) {
-            comparison = -1;
-        }
-        return order === "desc" ? comparison * -1 : comparison;
-    };
-};
-
-const orderArrayByKey = memoize((array, orderKey, order) => {
-    if (
-        Array.isArray(array) &&
-        array.length > 0 &&
-        orderKey !== false &&
-        order !== false &&
-        typeof orderKey === "string" &&
-        typeof array[0][orderKey] !== "undefined" &&
-        typeof order === "string"
-    ) {
-        array.sort(compareValues(orderKey, order));
-    }
-
-    return array;
-});
-
-const searchResultFilter = memoize(
-    (result = [], count = 0, offset = 0, orderKey = false, order = false) => {
-        if (count !== false && offset !== false) {
-            count = parseInt(count);
-            offset = parseInt(offset);
-        }
-
-        if (
-            Array.isArray(result) === false ||
-            result.length === 0 ||
-            typeof count !== "number" ||
-            typeof offset !== "number" ||
-            count === 0 ||
-            offset >= result.length
-        ) {
-            return [];
-        }
-
-        let sortResult = orderArrayByKey(result, orderKey, order);
-        sortResult = sortResult.slice(offset, offset + count);
-        return sortResult;
-    }
-);
-
-const createDatabaseList = (database = {}) => {
-    return (collection = "") => {
-        if (
-            typeof collection !== "string" ||
-            typeof database[collection] === "undefined" ||
-            collection === ""
-        ) {
-            return [];
-        }
-
-        const result = database[collection];
         return result;
     };
 };
 
 module.exports = {
-    createDatabaseSearch,
-    searchResultFilter,
-    createDatabaseList
+    createDatabaseSearch
 };
