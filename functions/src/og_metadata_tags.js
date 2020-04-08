629d3fc0a3fee2d1426965102ab4366d1ce8957f
hlolli <hlolli@gmail.com>
2020-04-09 00:45:21 +0200

    debug cloud function

--- functions/src/og_metadata_tags.js
+++ functions/src/og_metadata_tags.js
@@ -1,14 +1,16 @@
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
+const log = require("./logger.js")("og_metadata_tags");
 const isBot = require("isbot");
 const fs = require("fs");
 const R = require("ramda");
 
 exports.host = functions.https.onRequest(async (req, res) => {
     let indexHTML = fs.readFileSync("./index.html").toString();
+    log("debug", `indexHTML: ${indexHTML}`);
     const path = req.path ? req.path.split("/") : req.path;
     const ogPlaceholder = '<meta name="functions-insert-dynamic-og"/>';
-
+    log("debug", `path: ${path}`);
     if (
         //isBot(req.headers["user-agent"]) &&
         path &&
@@ -16,17 +18,20 @@ exports.host = functions.https.onRequest(async (req, res) => {
         path[1] === "editor"
     ) {
         const projectUid = path[2];
+        log("debug", `projectUid: ${projectUid}`);
         const project = await admin
             .firestore()
             .collection("projects")
             .doc(projectUid);
+        log("debug", `project: ${project}`);
         if (R.isNil(project)) {
             return res.status(404).send();
         }
         const profile = await admin
             .firestore()
-            .collection("projects")
+            .collection("profiles")
             .doc(projectUid.username);
+        log("debug", `profile: ${profile}`);
         if (R.isNil(profile)) {
             return res.status(404).send();
         }
@@ -36,10 +41,11 @@ exports.host = functions.https.onRequest(async (req, res) => {
             getProjectOg(projectWithUid, profile)
         );
     } else {
+        log("debug", `ELSE`);
         indexHTML = indexHTML.replace(ogPlaceholder, "");
         // res.status(200).send(indexHTML);
     }
-
+    log("debug", `return 200`);
     res.status(200).send(indexHTML);
 });
 
