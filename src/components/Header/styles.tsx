6c08d6f8935ab3b2bb2f39dd166af1862658d98e
Steven Yi <stevenyi@gmail.com>
2020-07-21 16:06:11 -0400

    added download button for non-owned projects [closes #91]

--- src/components/Header/styles.tsx
+++ src/components/Header/styles.tsx
@@ -1,5 +1,6 @@
 import { css as classCss } from "emotion";
 import { css } from "@emotion/core";
+import { shadow } from "@styles/_common";
 
 const drawerWidth = 260;
 
@@ -173,4 +174,25 @@ export const projectProfileTooltip = css`
 
 export const drawerIcon = css`
     fill: #fff;
+`;
+
+export const buttonContainer = theme => css`
+    position: relative;
+    top: 0;
+    color: ${theme.headerTextColor};
+    border: 2px solid ${theme.highlightBackground};
+    cursor: pointer;
+    border-radius: 3px;
+    ${shadow}
+    height: 42px;
+    width: auto;
+    margin: 0;
+    margin-left: 6px;
+    &:hover {
+        cursor: pointer;
+        border: 2px solid ${theme.line};
+        & > button {
+            border-color: transparent transparent transparent ${theme.line};
+        }
+    }
 `;
