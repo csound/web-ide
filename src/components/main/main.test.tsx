b59ea719c44ee34c75bd5af2f9ec1d5e9769521a
hlolli <hlolli@gmail.com>
2020-11-11 19:00:08 +0100

    jsdom changes in test

--- src/components/main/main.test.tsx
+++ src/components/main/main.test.tsx
@@ -8,12 +8,19 @@ import { JSDOM } from "jsdom";
 const { store, history } = configureStore();
 
 jest.mock("perfect-scrollbar");
-window.scrollTo = jest.fn();
 
-// https://discuss.codemirror.net/t/working-in-jsdom-or-node-js-natively/138/5
-const document_ = new JSDOM("<!doctype html><html><body></body></html>");
-global.document = document_;
-global.window = document_.defaultView;
+const { window } = new JSDOM("<!doctype html><html><body></body></html>", {
+    pretendToBeVisual: true,
+    features: {
+        FetchExternalResources: ["script", "css"],
+        QuerySelector: true
+    },
+    resources: "usable"
+});
+
+global.document = window.document;
+global.window = window;
+global.scrollTo = jest.fn();
 
 it("renders without crashing", () => {
     const div = global.document.createElement("div");
