363eefce2b45ebf461b3348068aa5211ee7d2ebe
Hlöðver Sigurðsson <hlolli@gmail.com>
2022-01-31 13:44:54 +0200

    fix build

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -326,7 +326,7 @@ module.exports = function (webpackEnv = "production") {
                     // Bump up the default maximum size (2mb) that's precached,
                     // to make lazy-loading failure scenarios less likely.
                     // See https://github.com/cra-template/pwa/issues/13#issuecomment-722667270
-                    maximumFileSizeToCacheInBytes: 5 * 1024 * 1024
+                    maximumFileSizeToCacheInBytes: 10 * 1024 * 1024
                 }),
             new ForkTsCheckerWebpackPlugin({
                 async: isEnvDevelopment,
