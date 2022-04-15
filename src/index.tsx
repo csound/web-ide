89243cbb0c1dcc114a8bfc8fc85a43b3023fa3c8
Hlöðver Sigurðsson <hlolli@gmail.com>
2022-04-15 17:21:06 +0200

    adapt to react18

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { Provider as Provider_ } from "react-redux";
+import { createRoot } from "react-dom/client";
 import Main from "./components/main/main";
+import { store } from "@store";
 
 import "./config/firestore"; // import for sideffects
 import "react-perfect-scrollbar/dist/css/styles.css";
 
-// INITIALIZE REACT RENDERING
-ReactDOM.render(<Main />, document.querySelector("#root"));
+/* eslint-disable-next-line unicorn/prefer-query-selector */
+const container = document.getElementById("root");
+const root = createRoot(container as any);
+const Provider = Provider_ as any;
+root.render(
+    <Provider store={store}>
+        <Main />
+    </Provider>
+);
