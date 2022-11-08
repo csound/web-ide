3d7cd2cdc62a6f9234a6d83c2126d7211edf6842
Hlöðver Sigurðsson <hlolli@gmail.com>
2022-11-08 23:23:47 +0100

    feat: add csound7 syntax support to cm6

--- src/components/editor/modes/csound/debug-parser.tsx
+++ src/components/editor/modes/csound/debug-parser.tsx
@@ -5,20 +5,30 @@ import { printTree } from "./print-tree";
 
 const instrBlock = `
 
-opcode contains, i, ik[]
-  ival, karr[] xin
-  indx = 0
-  iret = 0
-  while (indx < lenarray:i(karr)) do
-    if (i(karr,indx) == ival) then
-      iret = 1
-      igoto end
-    endif
-    indx += 1
-  od
-end:
-  xout iret
-endop 
+
+<CsoundSynthesizer>
+<CsInstruments>
+sr = 44100
+ksmps = 1
+nchnls = 2
+0dbfs = 1
+struct TypeX val1:i
+declare myFun(arg1:TypeX):(TypeX)
+instr 1
+  varX:TypeX init 1
+  varY:TypeX = myFun(varX)
+  print(varY.val1)
+endin
+opcode myFun(arg1:TypeX):TypeX
+  retVal:TypeX init arg1.val1 + 1
+  xout(retVal)
+endop
+</CsInstruments>
+<CsScore>
+i1 0 0
+e
+</CsScore>
+</CsoundSynthesizer>
 
 `;
 
